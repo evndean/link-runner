cd815ea74245bd70678bcfee7e2faa3e48d0dd64
Evan Dean <evan.dean@controlgroup.com>
2015-08-14 13:52:55 -0400

    added comment

--- js/gameStates/Preloader.js
+++ js/gameStates/Preloader.js
@@ -8,6 +8,8 @@ LinkRunner.Preloader.prototype = {
 
 	preload: function () {
 
+		// If using a preloader progress bar, initialize it here
+
 		// Load the game assets
 		this.load.tilemap('map-01', 'assets/opengameart/tilemaps/json/map-01.json', null, Phaser.Tilemap.TILED_JSON);
 		this.load.image('dirt', 'assets/opengameart/tilemaps/tiles/dirt-platformer-tiles.png');
