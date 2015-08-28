ba5f2c4af6a99015ae96e7d094f32e616770f0f1
Evan Dean <evan.dean@controlgroup.com>
2015-08-28 16:12:18 -0400

    moved laser asset

--- src/gameStates/Preloader.js
+++ src/gameStates/Preloader.js
@@ -7,12 +7,16 @@ LinkRunner.Preloader.prototype.preload = function () {
 	// If using a preloader progress bar, initialize it here
 
 	// Load the game assets
+
+	// Tilemap stuff
 	this.load.tilemap('map-01', 'assets/tilemaps/json/map-01.json', null, Phaser.Tilemap.TILED_JSON);
 	this.load.tilemap('training-01', 'assets/tilemaps/json/training-01.json', null, Phaser.Tilemap.TILED_JSON);
 	this.load.image('dirt', 'assets/tilemaps/tiles/dirt-platformer-tiles.png');
 	this.load.image('pipe-walls', 'assets/tilemaps/tiles/pipe-walls.png');
-	this.load.image('lazerBeam', 'assets/phaser/games/invaders/bullet.png');  // Temporarily using a bullet image
+
+	// Player stuff
 	this.load.spritesheet('drone', 'assets/spritesheets/drone.png', 64, 26);
+	this.load.image('lazerBeam', 'assets/spritesheets/bullet.png');
 	this.load.audio('laser', 'assets/audio/33-laser.wav');
 	this.load.audio('crash', 'assets/audio/14-crash.wav');
 
