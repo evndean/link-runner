97f14e46c2b474c2f8af66e4a54db8bc4a9ae491
Evan Dean <evan.dean@controlgroup.com>
2015-08-28 16:09:55 -0400

    moved tilemaps

--- src/gameStates/Preloader.js
+++ src/gameStates/Preloader.js
@@ -7,10 +7,10 @@ LinkRunner.Preloader.prototype.preload = function () {
 	// If using a preloader progress bar, initialize it here
 
 	// Load the game assets
-	this.load.tilemap('map-01', 'assets/opengameart/tilemaps/json/map-01.json', null, Phaser.Tilemap.TILED_JSON);
-	this.load.tilemap('training-01', 'assets/opengameart/tilemaps/json/training-01.json', null, Phaser.Tilemap.TILED_JSON);
-	this.load.image('dirt', 'assets/opengameart/tilemaps/tiles/dirt-platformer-tiles.png');
-	this.load.image('pipe-walls', 'assets/opengameart/tilemaps/tiles/pipe-walls.png');
+	this.load.tilemap('map-01', 'assets/tilemaps/json/map-01.json', null, Phaser.Tilemap.TILED_JSON);
+	this.load.tilemap('training-01', 'assets/tilemaps/json/training-01.json', null, Phaser.Tilemap.TILED_JSON);
+	this.load.image('dirt', 'assets/tilemaps/tiles/dirt-platformer-tiles.png');
+	this.load.image('pipe-walls', 'assets/tilemaps/tiles/pipe-walls.png');
 	this.load.image('lazerBeam', 'assets/phaser/games/invaders/bullet.png');  // Temporarily using a bullet image
 	this.load.spritesheet('drone', 'assets/spritesheets/drone.png', 64, 26);
 	this.load.audio('laser', 'assets/audio/33-laser.wav');
